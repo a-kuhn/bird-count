74b3054af61bde20fcd8afe8e5a14808996b5f48
alex k <alex.kuhn1123@gmail.com>
2020-08-14 11:01:55 -0700

    add logout link

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import {Link} from '@reach/router';
 
 export default () => {
 
     return(
+        <div>
+            <Link to="/logout">Logout</Link>
         <p>Main Page!! this will hold the SearchForm, CreateChecklist, MyChecklists, and SingleChecklist components and views</p>
+        </div>
     );
 }
