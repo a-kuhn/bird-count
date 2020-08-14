230d02a80c8a20a9567f831be5240a9bb0c442ff
alex k <alex.kuhn1123@gmail.com>
2020-08-14 10:39:45 -0700

    create main view

---
+++ client/src/views/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => {
+
+    return(
+        <p>Main Page!! this will hold the SearchForm, CreateChecklist, MyChecklists, and SingleChecklist components and views</p>
+    );
+}
