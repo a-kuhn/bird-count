ab632a4d42b50831a4e5671c1a8088f47067d8d9
alex k <alex.kuhn1123@gmail.com>
2020-08-14 10:37:47 -0700

    add LoginReg view to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Router} from '@reach/router';
 import './App.css';
+import LoginReg from './views/LoginReg';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <LoginReg path="/" />
+        
+      </Router>
     </div>
   );
 }
