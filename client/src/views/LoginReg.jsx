f1483e6b2d7950d027309760d37e6e5452f63d61
alex k <alex.kuhn1123@gmail.com>
2020-08-14 10:44:28 -0700

    switch <span> to <Link> and path to LoginReg view

--- client/src/views/LoginReg.jsx
+++ client/src/views/LoginReg.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from '@reach/router';
 
 export default () => {
 
@@ -6,7 +7,7 @@ export default () => {
         <div>
             <h1>Welcome to Bird Count!</h1>
             <p>future development will be to add login and registration functionality</p>
-            <h3>For now, please, <span className="btn btn-outline-success btn-lg">continue as a guest</span></h3>
+            <h3>For now, please, <Link to="/main" className="btn btn-outline-success btn-lg">continue as a guest</Link></h3>
         </div>
     );
 }
