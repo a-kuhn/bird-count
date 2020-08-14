bfee27ade4e0ece40592e77e294c9050cdb96437
alex k <alex.kuhn1123@gmail.com>
2020-08-14 10:35:12 -0700

    create login/reg view

---
+++ client/src/views/LoginReg.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+
+    return(
+        <div>
+            <h1>Welcome to Bird Count!</h1>
+            <p>future development will be to add login and registration functionality</p>
+            <h3>For now, please, <span className="btn btn-outline-success btn-lg">continue as a guest</span></h3>
+        </div>
+    );
+}
