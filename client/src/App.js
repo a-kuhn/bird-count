22ecf5effbc6b0dc2fbaf7754d4c0052c6677c30
alex k <alex.kuhn1123@gmail.com>
2020-08-14 10:43:55 -0700

    add main and logout views to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Router} from '@reach/router';
 import './App.css';
 import LoginReg from './views/LoginReg';
+import Main from './views/Main';
+import Logout from './views/Logout';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <LoginReg path="/" />
-        
+        <Main path="/main" />
+        <Logout path="/logout"/>
       </Router>
     </div>
   );
