b2006dbfaccdeca510306e943f2d103e04081555
alex k <alex.kuhn1123@gmail.com>
2020-08-16 16:38:33 -0700

    chained API call is working! trying to display some results

--- client/src/components/DisplayResults.jsx
+++ client/src/components/DisplayResults.jsx
@@ -4,26 +4,26 @@ import axios from 'axios';
 export default ({localeString, queryStringPartial, seasonFilter}) => {
     // create variables for props passed down from Main.jsx 
     const locale = localeString;
-    let queryString;
     const season = seasonFilter;
+    let queryString;
     // create state for results of API calls:
     const [birdList, setBirdList] = useState([]);
     const [geocodeError, setGeocodeError] = useState('');
     const [birdListError, setBirdListError] = useState('');
-
+    
     // make calls to Geocoder then iNaturalist
     useEffect(()=>{
+
         axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${locale}&key=${process.env.REACT_APP_GEOCODER_KEY}`)
             .then(res=>{
-                queryString = `${queryStringPartial}&lat=${res.data.results[0].geometry.location.lat}&lng=${res.data.results[0].geometry.location.lng}`;
-                console.log(`queryStringFinal: ${queryString}`);
+                // use response to build request to iNaturalist:
+                // queryString = `${queryStringPartial}&lat=${res.data.results[0].geometry.location.lat}&lng=${res.data.results[0].geometry.location.lng}`;
+
+                axios.get(`https://api.inaturalist.org/v1/observations${queryStringPartial}&lat=${res.data.results[0].geometry.location.lat}&lng=${res.data.results[0].geometry.location.lng}`)
+                    .then(res=>{setBirdList(res.data); console.log(res.data);})
+                    // .then(filterBirdList(birdList))
+                    .catch(err => setBirdListError(`an error occurred while trying to build your checklist:\n${err}`))
             })
-            // .then(
-            //     axios.get(`https://api.inaturalist.org/v1/observations${queryString}`)
-            //         .then(res=>setBirdList(res.data))
-            //         .then(filterBirdList(birdList))
-            //         .catch(err => setBirdListError(`an error occurred while trying to build your checklist`))
-            // )
             .catch(err=>setGeocodeError(`something's wrong with the location you're using:\n${err}`))
     },[locale]);
 
@@ -40,6 +40,13 @@ export default ({localeString, queryStringPartial, seasonFilter}) => {
             <h4>locale: {locale}</h4>
             <h4>queryString: {queryString}</h4>
             <h4>season: {season}</h4>
+
+            <h4>unfiltered list:</h4>
+            <ul>
+                { birdList.map((bird, idx)=>
+                <li key={idx}>{bird.id}</li>
+                )}
+            </ul>
 
         </div>
     );
