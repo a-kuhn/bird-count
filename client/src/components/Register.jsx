3725b1802302e878226e226af356a8fa0ac8d749
alex k <alex.kuhn1123@gmail.com>
2020-08-21 16:29:35 -0700

    start register form

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+export default () => {
+    // create state for tracking form input
+
+    // handle submit -- eventually send post req to DB 
+
+    return(
+        <>
+            register form
+        </>
+    );
+}
