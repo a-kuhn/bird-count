5e4e02fb78d077dc43b1847145846930719570ce
alex k <alex.kuhn1123@gmail.com>
2020-08-27 17:31:34 -0700

    build out register form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,13 +1,86 @@
 import React, {useState} from 'react';
+import {navigate} from '@reach/router';
 
 export default () => {
-    // create state for tracking form input
+  //to keep track of what is being typed into search form
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [pwConfirm, setPwConfirm] = useState("");
 
-    // handle submit -- eventually send post req to DB 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //TODO: build post request for db to create new user
+  } 
 
     return(
         <>
-            register form
-        </>
+    {/* main container for form */}
+    <form className="container dark-font" onSubmit={handleSubmit}>
+        {/* top row for location and season columns */}
+        <div className="form-row justify-content-around">
+            {/* left column for location */}
+            <fieldset className="thick-border form-group col-md-5">
+            <legend className="w-auto mx-3 px-1">register</legend>
+
+                <div className="form-row m-1">
+                    <div className="col-md-6">
+                        <input
+                            onChange={(e) => setFirstName(e.target.value)}
+                            value={firstName}
+                            type="text"
+                            className="form-control"
+                            placeholder="first name"
+                        ></input>
+                    </div>
+                    <div className="col-md-6">
+                        <input
+                            onChange={(e) => setLastName(e.target.value)}
+                            value={lastName}
+                            type="text"
+                            className="form-control"
+                            placeholder="last name"
+                        ></input>
+                    </div>
+                </div>
+                <div className="form-row m-2 m-2">
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="text"
+                        className="form-control"
+                        placeholder="email"
+                    ></input>
+                </div>
+                <div className="form-row m-1">
+                    <div className="col-md-6">
+                        <input
+                            onChange={(e) => setPw(e.target.value)}
+                            value={pw}
+                            type="text"
+                            className="form-control"
+                            placeholder="password"
+                        ></input>
+                    </div>
+                    <div className="col-md-6">
+                        <input
+                            onChange={(e) => setPwConfirm(e.target.value)}
+                            value={pwConfirm}
+                            type="text"
+                            className="form-control"
+                            placeholder="confirm password"
+                        ></input>
+                    </div>
+                </div>
+                <div className="row justify-content-around">
+                    <button className="btn btn-outline-success thick-border dark-font font-weight-bold m-2">
+                        register
+                    </button>
+                </div>    
+            </fieldset> 
+        </div>
+    </form>
+    </>
     );
 }
--- client/src/views/LoginReg.jsx
+++ client/src/views/LoginReg.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from '@reach/router';
 
+import Register from '../components/Register';
+
 export default () => {
 
     return(
@@ -8,6 +10,7 @@ export default () => {
             <h1>Welcome to Bird Count!</h1>
             <p>future development will be to add login and registration functionality</p>
             <h3>For now, please, <Link to="/main" className="btn btn-outline-success btn-lg">continue as a guest</Link></h3>
+            <Register/>
         </div>
     );
 }
