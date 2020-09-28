f1677883832b7b20155d3af63a4187207dd57a04
alex k <alex.kuhn1123@gmail.com>
2020-09-28 16:45:16 -0700

    add req.body parsing middleware functions

--- server/server.js
+++ server/server.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// to run server on specific port:
 app.listen( port, () => console.log(`listening on port: ${port}`));
+
+// Express middleware functions responsible for providing and parsing req.body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
