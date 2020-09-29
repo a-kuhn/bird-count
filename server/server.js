5e413708b0e66c1e0dfa51ca01a817d3cc8ab9b8
alex k <alex.kuhn1123@gmail.com>
2020-09-28 18:40:27 -0700

    add cors, db_name, routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const port = 8000;
+const db_name = "birdCount";
 
-// to run server on specific port:
-app.listen( port, () => console.log(`listening on port: ${port}`));
+// const mongooseConfigFunc = require("./config/mongoose.config");
+// mongooseConfigFunc(db_name);
+// shorthand of above
+require("./config/mongoose.config")(db_name);
 
-// Express middleware functions responsible for providing and parsing req.body data
+const app = express();
+
+// Prevent CORS error when making request from react port 3000 to server port 8000
+// Error in chrome console: Access to XMLHttpRequest at...
+app.use(cors());
+
+// req.body undefined without this!
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+require("./routes/city.routes")(app);
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port} for REQuests to RESpond to.`),
+);
