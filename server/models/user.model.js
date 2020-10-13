1d0f4ee40fb7abc484fd6b8b080f324c2ec3d980
alex k <alex.kuhn1123@gmail.com>
2020-10-13 16:11:31 -0700

    add password hashing back in

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -45,12 +45,12 @@ UserSchema.pre("validate", function (next) {
   next();
 });
 
-// UserSchema.pre("save", function (next) {
-//   bcrypt.hash(this.password, 10).then((hash) => {
-//     this.password = hash;
-//     next();
-//   });
-// });
+UserSchema.pre("save", function (next) {
+  bcrypt.hash(this.password, 10).then((hash) => {
+    this.password = hash;
+    next();
+  });
+});
 
 const User = mongoose.model("User", UserSchema);
 
