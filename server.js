5c0277b7b47622137be1f3b6907729c0b100a680
alex k <alex.kuhn1123@gmail.com>
2020-10-14 14:06:41 -0700

    clean up

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
+
 const express = require("express"),
   cookieParser = require("cookie-parser"),
   cors = require("cors");
 
-
-// const mongooseConfigFunc = require("./config/mongoose.config");
-// mongooseConfigFunc(db_name);
-// shorthand of above
-require("./server/config/mongoose.config.js")(process.env.DB_NAME);
+require("./server/config/mongoose.config")(process.env.DB_NAME);
 
 const app = express();
 
 app.use(cookieParser());
-// Prevent CORS error when making request from react port 3000 to server port 8000
-// Error in chrome console: Access to XMLHttpRequest at...
-app.use(cors({credentials: true, origin: "http://localhost:3000"}));
-
-// req.body undefined without this!
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("./server/routes/users.routes.js")(app);
+require("./server/routes/users.routes")(app);
 
 app.listen(process.env.DB_PORT, () =>
-  console.log(`Listening on port ${process.env.DB_PORT} for REQuests to RESpond to.`),
+  console.log(`Listening on port ${process.env.DB_PORT}`)
 );
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,18 +1,12 @@
-
 const mongoose = require("mongoose");
 
-// export an arrow function so that our server.js can pass in the db_name
-module.exports = (db_name) => {
+module.exports = db_name => {
   mongoose
     .connect(`mongodb://localhost/${db_name}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
+      useFindAndModify: false
     })
-    .then(() => {
-      console.log(`Successfully connect to ${db_name}`);
-    })
-    .catch((err) => {
-      console.log(`mongoose connection to ${db_name} failed`, err);
-    });
+    .then(() => console.log(`Successfully connected to ${db_name}`))
+    .catch(err => console.log("mongoose connection failed: ", err));
 };
