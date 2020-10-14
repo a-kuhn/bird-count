833bfc807b9ff2655612d140892d13eb0cb1e905
alex k <alex.kuhn1123@gmail.com>
2020-10-14 13:22:57 -0700

    import authenticate method from jwt.config

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,4 +1,6 @@
 const UsersController = require('../controllers/users.controller');
+// can pass authenticate as 2nd param to any route that user needs to be logged in to access
+const {authenticate} = require('../config/jwt.config'); 
 
 module.exports = app => {
     //get all users
