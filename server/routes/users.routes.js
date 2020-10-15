0b3657f0a6f03c8811bc45873de88a7eabaaedba
alex k <alex.kuhn1123@gmail.com>
2020-10-15 14:54:41 -0700

    rebuild routes, adding authenticate

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,16 +1,15 @@
-const UsersController = require('../controllers/users.controller');
-// can pass authenticate as 2nd param to any route that user needs to be logged in to access
-const {authenticate} = require('../config/jwt.config'); 
+const userController = require("../controllers/users.controller");
+const { authenticate } = require("../config/jwt.config");
 
 module.exports = app => {
-    //get all users
-    app.get("/api/users", UsersController.getAllUsers);
-    //get single users
-    app.get("/api/users/:id", UsersController.getSingleUser);
-    //create new user
-    app.post("/api/users/new", UsersController.register);
-    //update existing user
-    app.post("/api/users/update/:id", UsersController.updateUser);
-    //delete single user
-    app.delete("/api/users/delete/:id", UsersController.deleteUser);
-}
+  app.post("/api/register", userController.register);
+  app.post("/api/login", userController.login);
+  app.post("/api/logout", userController.logout);
+
+  // this route now has to be authenticated
+  app.get("/api/users", authenticate, userController.getAllUsers);
+  app.get("/api/users/loggedin", authenticate, userController.getLoggedInUser);
+  // app.post("/api/users/update/:id", UsersController.updateUser);
+  // app.delete("/api/users/delete/:id", UsersController.deleteUser);
+
+};
