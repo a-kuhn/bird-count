d945bf4bbb01f7e4cffee85fd2281111473b34d0
alex k <alex.kuhn1123@gmail.com>
2020-10-15 14:48:11 -0700

    add update and delete methods

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -4,79 +4,91 @@ const jwt = require("jsonwebtoken");
 
 // export an object that is full of methods
 module.exports = {
-  register(req, res) {
-    const user = new User(req.body);
+    register(req, res) {
+        const user = new User(req.body);
 
-    user
-      .save()
-      .then(() => {
-        res.json({ msg: "success!", user: user });
-      })
-      .catch((err) => res.status(400).json(err));
-  },
+        user
+        .save()
+        .then(() => {
+            res.json({ msg: "success!", user: user });
+        })
+        .catch((err) => res.status(400).json(err));
+    },
 
-  login(req, res) {
-    User.findOne({ email: req.body.email })
-      .then((user) => {
-        if (user === null) {
-          res.status(400).json({ msg: "invalid login attempt" });
-        } else {
-          bcrypt
-            .compare(req.body.password, user.password)
-            .then((passwordIsValid) => {
-              if (passwordIsValid) {
-                res
-                  .cookie(
-                    "usertoken",
-                    jwt.sign({ _id: user._id }, process.env.JWT_SECRET),
-                    {
-                      httpOnly: true,
-                    }
-                  )
-                  .json({ msg: "success!" });
-              } else {
-                res.status(400).json({ msg: "invalid login attempt" });
-              }
-            })
-            .catch((err) =>
-              res.status(400).json({ msg: "invalid login attempt" })
-            );
-        }
-      })
-      .catch((err) => res.json(err));
-  },
+    login(req, res) {
+        User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user === null) {
+            res.status(400).json({ msg: "invalid login attempt" });
+            } else {
+            bcrypt
+                .compare(req.body.password, user.password)
+                .then((passwordIsValid) => {
+                if (passwordIsValid) {
+                    res
+                    .cookie(
+                        "usertoken",
+                        jwt.sign({ _id: user._id }, process.env.JWT_SECRET),
+                        {
+                        httpOnly: true,
+                        }
+                    )
+                    .json({ msg: "success!" });
+                } else {
+                    res.status(400).json({ msg: "invalid login attempt" });
+                }
+                })
+                .catch((err) =>
+                res.status(400).json({ msg: "invalid login attempt" })
+                );
+            }
+        })
+        .catch((err) => res.json(err));
+    },
 
-  logout(req, res) {
-    res
-      .cookie("usertoken", jwt.sign({ _id: "" }, process.env.JWT_SECRET), {
-        httpOnly: true,
-        maxAge: 0,
-      })
-      .json({ msg: "ok" });
-  },
+    logout(req, res) {
+        res
+        .cookie("usertoken", jwt.sign({ _id: "" }, process.env.JWT_SECRET), {
+            httpOnly: true,
+            maxAge: 0,
+        })
+        .json({ msg: "ok" });
+    },
 
-  logout2(req, res) {
-    res.clearCookie("usertoken");
-    res.json({ msg: "usertoken cookie cleared" });
-  },
+    logout2(req, res) {
+        res.clearCookie("usertoken");
+        res.json({ msg: "usertoken cookie cleared" });
+    },
 
-  getLoggedInUser(req, res) {
-    const decodedJWT = jwt.decode(req.cookies.usertoken, { complete: true });
+    getLoggedInUser(req, res) {
+        const decodedJWT = jwt.decode(req.cookies.usertoken, { complete: true });
 
-    User.findById(decodedJWT.payload._id)
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
-  },
+        User.findById(decodedJWT.payload._id)
+        .then((user) => res.json(user))
+        .catch((err) => res.json(err));
+    },
 
-  getAllUsers(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.json(err));
-  },
+    getAllUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.json(err));
+    },
 
-  getSingleUser(req, res) {
-    User.findOne({ _id: req.params.id })
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
-  },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.id })
+        .then((user) => res.json(user))
+        .catch((err) => res.json(err));
+    },
+    // /api/users/edit/:id  --> updateUser
+    updateUser(req, res) {
+        User.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+            .then(updatedUser => res.json({updatedUser: updatedUser}))
+            .catch(err => res.status(400).json(err))
+    },
+    // /api/users/delete/:id  --> deleteUser
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.id})
+            .then(result => res.json({result: result}))
+            .catch(err => res.json({message: "Something went wrong with deleteUser", error: err}))
+    }
 };
