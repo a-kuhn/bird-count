1e3c6f10da6159e4139078ad2f718d2753c21d5a
alex k <alex.kuhn1123@gmail.com>
2020-10-16 15:23:44 -0700

    replace logout link with functional button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,9 +3,19 @@ import axios from 'axios';
 import {Link, navigate} from '@reach/router';
 
 export default () => {
+    const logout = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:8000/api/logout')
+            .then(res => {
+                console.log(`res from NavBar logout: ${res}`);
+                navigate('/logout')
+            })
+    };
+
     return(
         <div className="container">
-            <Link to="/logout" className="btn btn-outline-danger btn-lg m-4">Logout</Link>
+            <button onClick={logout} className="btn btn-outline-danger btn-lg m-4">Logout</button>
             <Link to="/home" className="btn btn-outline-primary btn-lg m-4">Home</Link>
         </div>
     );
