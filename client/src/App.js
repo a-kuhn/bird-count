c9b87ca82efb4a171503aebf84cf393631ac39b1
alex k <alex.kuhn1123@gmail.com>
2020-10-16 15:12:15 -0700

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
       <Router path="/">
         <LoginReg path="/">
-          <Register path=""/>
         </LoginReg>
         <Main path="main">
           <SearchForm path=""/>
