67b75e275a514c1b9670c29c3899fbfa3a111340
alex k <alex.kuhn1123@gmail.com>
2020-10-15 17:01:14 -0700

    add express configuration

--- server/server.js
+++ server/server.js
@@ -11,9 +11,11 @@ const app = express();
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 require("./routes/users.routes")(app);
 
 app.listen(process.env.DB_PORT, () =>
   console.log(`Listening on port ${process.env.DB_PORT}`)
 );
+
