30909c051712be710fcbb8dbe2f3548597e60041
alex k <alex.kuhn1123@gmail.com>
2020-10-19 16:54:25 -0700

    cleanup

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -68,15 +68,4 @@ module.exports = {
       .catch((err) => res.json(err));
   },
 
-  getAll(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.json(err));
-  },
-
-  getOne(req, res) {
-    User.findOne({ _id: req.params.id })
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
-  },
 };
