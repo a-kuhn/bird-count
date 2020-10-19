f93cbc56ad83d7b1a5fdc3c65f9f687a8256b1c2
alex k <alex.kuhn1123@gmail.com>
2020-10-19 16:54:47 -0700

    cleanup

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,11 +3,14 @@ const userController = require("../controllers/user.controller");
 const { authenticate } = require("../config/jwt.config");
 
 module.exports = app => {
+  // REGISTER
   app.post("/api/register", userController.register);
+  // LOGIN
   app.post("/api/login", userController.login);
+  // LOGOUT
   app.post("/api/logout", userController.logout);
 
-  // this route now has to be authenticated
-  app.get("/api/users", authenticate, userController.getAll);
+  // these routes have to be authenticated with JWT
+  // GET LOGGED IN USER
   app.get("/api/users/loggedin", authenticate, userController.getLoggedInUser);
 };
