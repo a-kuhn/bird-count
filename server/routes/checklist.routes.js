61711341e349acf510384563512a99fd3d6c51b8
alex k <alex.kuhn1123@gmail.com>
2020-10-19 16:54:39 -0700

    create checklist routes

---
+++ server/routes/checklist.routes.js
@@ -0,0 +1,9 @@
+
+
+//create new checklist /api/checklists/new
+
+//get one checklist  /api/checklists/:id
+
+//update checklist  /api/checklists/update/:id
+
+//delete checklist  /api/checklists/delete/:id
