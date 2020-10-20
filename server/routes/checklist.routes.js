51b0719fb023bcfab3f9b88781331f94ff142a19
alex k <alex.kuhn1123@gmail.com>
2020-10-19 17:28:52 -0700

    cleanup

--- server/routes/checklist.routes.js
+++ server/routes/checklist.routes.js
@@ -6,8 +6,8 @@ module.exports = app => {
 //create new checklist
   app.post("/api/checklists/new", authenticate, checklistController.create);
 
+  //get one checklist  /api/checklists/:id
 
-//get one checklist  /api/checklists/:id
 
 //update checklist  /api/checklists/update/:id
 
