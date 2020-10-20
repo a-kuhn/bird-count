5863225720b23180f4bdfd34686b42ae0dc9a12c
alex k <alex.kuhn1123@gmail.com>
2020-10-19 17:25:25 -0700

    add route to create checklist

--- server/routes/checklist.routes.js
+++ server/routes/checklist.routes.js
@@ -1,9 +1,16 @@
 
+const checklistController = require("../controllers/checklist.controller");
+const { authenticate } = require("../config/jwt.config");
 
-//create new checklist /api/checklists/new
+module.exports = app => {
+//create new checklist
+  app.post("/api/checklists/new", authenticate, checklistController.create);
+
 
 //get one checklist  /api/checklists/:id
 
 //update checklist  /api/checklists/update/:id
 
 //delete checklist  /api/checklists/delete/:id
+
+}
