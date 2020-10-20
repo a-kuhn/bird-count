c1588dc754abca705d2414e4b4d373e2a07aee6e
alex k <alex.kuhn1123@gmail.com>
2020-10-20 15:14:46 -0700

    map over checklists to display ChecklistLinks

--- client/src/components/ChecklistLink.jsx
+++ client/src/components/ChecklistLink.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 export default ({checklist}) => {
+    const formatted_date = checklist.updatedAt;
 
     return(
         <>
-        <div className="card mb-3 mr-3 thick-border" style={{width: 20 + 'em'}} >
+        <div className="card mb-3 mr-3 thick-border d-flex" style={{width: 20 + 'em'}} >
             <div className="card-body text-center">
                 <a 
                     href={`http://localhost:8000/checklists/${checklist._id}`}
-                    className="h5 py-3"
-                    stretched-link
+                    className="h5 py-3 stretched-link"
                 >{checklist.title}
                 </a>
                 <p className="font-italic">{checklist.location}</p>
-                <a href={`${bird.iNatOccUrl}`}><span className="h6 dark-font" style={{fontSize: 14 + 'px'}}>last seen here on: </span>{formatted_date}</a>
+                <p><span className="h6 dark-font" style={{fontSize: 14 + 'px'}}>last updated: </span>{formatted_date}</p>
             </div>
         </div>
         </>
