4349be296243a917f1de97ee4a6d6de9d4b891f9
alex k <alex.kuhn1123@gmail.com>
2020-10-20 15:46:24 -0700

    formatting

--- client/src/components/ChecklistLink.jsx
+++ client/src/components/ChecklistLink.jsx
@@ -9,21 +9,23 @@ export default ({checklist}) => {
                 <div className="card-body justify-content-between d-flex">
                     <a 
                         href={`http://localhost:8000/checklists/${checklist._id}`}
-                        className="h2 py-3 stretched-link d-inline-flex"
+                        className="h2 py-3 stretched-link d-inline-flex col-8"
                     >{checklist.title}
                     </a>
-                    <p 
-                        className="font-bold d-inline-flex align-self-center" 
-                        style={{fontSize: 22 + 'px'}}
-                    >location: 
-                        <span>{checklist.location}</span>
-                    </p>
-                    <p 
-                        className="h6 dark-font d-inline-flex align-self-center mr-4" 
-                        style={{fontSize: 14 + 'px'}}
-                    >last updated: 
-                        <span>{formatted_date}</span>
-                    </p>
+                    <div className="col-4">
+                        <p 
+                            className="font-bold d-inline-flex align-self-center" 
+                            style={{fontSize: 22 + 'px'}}
+                        >location: 
+                            <span>{checklist.location}</span>
+                        </p>
+                        <p 
+                            className="h6 dark-font d-inline-flex align-self-center mr-4" 
+                            style={{fontSize: 14 + 'px'}}
+                        >last updated: 
+                            <span>{formatted_date}</span>
+                        </p>
+                    </div>
                 </div>
             </div>
             <div className="thick-border col-2 d-inline-flex">future delete button</div>
