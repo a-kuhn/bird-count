83a6a75324a53e9f418dd929e797ccef81d9e079
alex k <alex.kuhn1123@gmail.com>
2020-10-19 17:43:30 -0700

    debug & test create checklist

--- server/controllers/checklist.controller.js
+++ server/controllers/checklist.controller.js
@@ -6,15 +6,19 @@ const jwt = require("jsonwebtoken");
 module.exports = {
     // create a new checklist && add newChecklist._id to user.checklists
     create(req, res) {
+        console.log(`creating a new checklist!\n${req.body}`);
         const checklist = new Checklist(req.body);
 
         const decodedJWT = jwt.decode(req.cookies.usertoken, {complete: true});
+        console.log(`userID: ${decodedJWT.payload._id}`);
 
         checklist.save()
             .then(newList => {
+                console.log(`adding newChecklist to db:\n${newList}`);
                 User.findByIdAndUpdate(decodedJWT.payload._id, 
                     { $push: {checklists: newList._id}},
                     {new: true, useFindAndModify: false});
+                res.json(newList);
             })
             .catch(err => res.json(err));
     }
