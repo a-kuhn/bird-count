71f6cdd35bd75bcc443d64bcb55dcdea2220f349
alex k <alex.kuhn1123@gmail.com>
2020-10-20 12:15:49 -0700

    switch to logout2

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -8,7 +8,7 @@ module.exports = app => {
   // LOGIN
   app.post("/api/login", userController.login);
   // LOGOUT
-  app.post("/api/logout", userController.logout);
+  app.post("/api/logout", userController.logout2);
 
   // these routes have to be authenticated with JWT
   // GET LOGGED IN USER
