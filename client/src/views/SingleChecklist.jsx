1f895e43659b5e23705271fac17e1f0fa613993a
alex k <alex.kuhn1123@gmail.com>
2020-10-21 10:19:48 -0700

    get checklist from db

--- client/src/views/SingleChecklist.jsx
+++ client/src/views/SingleChecklist.jsx
@@ -6,16 +6,16 @@ import NavBar from '../components/NavBar';
 
 
 export default ({checklistId}) => {
-    console.log(`SINGLE CHECKLIST*************\nchecklistId: ${checklistId}`);
     // GET CHECKLIST
     const [checklist, setChecklist] = useState([]);
     useEffect(() => {
         axios.get(`http://localhost:8000/api/checklists/${checklistId}`, {withCredentials: true})
             .then(res => {
-                console.log(`response from getOne checklist: ${res.data}`);
+                console.log(`response from getOne checklist: ${res.data.title}`);
+                setChecklist(res.data);
             })
             .catch(err => console.log(err));
-    });
+    },[]);
 
     return(
         <div className="container">
@@ -23,6 +23,9 @@ export default ({checklistId}) => {
 
             <div>
                 <p>placeholder to display single checklist as form to be updated by user clicking bird card to toggle hasBeenSeen property</p>
+                {/* create form & map over checklist like in DisplayResults */}
+                {/* add Title, Location, Notes fields */}
+                
             </div>
         </div>
     );
