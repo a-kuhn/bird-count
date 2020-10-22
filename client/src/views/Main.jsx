481f3258b54fc5ce591544195fe3ee29314837e2
alex k <alex.kuhn1123@gmail.com>
2020-10-22 14:51:11 -0700

    add userName to main navbar

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link, Router} from '@reach/router';
+import axios from 'axios';
 
 import NavBar from '../components/NavBar';
 import SearchForm from '../components/SearchForm';
 import DisplayResults from '../components/DisplayResults';
 
 export default () => {
+    // GET LOGGED IN USER:
+    const [loggedInUser, setLoggedInUser] = useState({});
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/users/loggedin", {withCredentials: true})
+            .then(user => {
+                console.log(`user: ${user.data.email}`)
+                setLoggedInUser(user.data);
+            })
+            .catch(err => {console.log(err)}); 
+    }, []);
 
     return(
         <div className="container">
-            <NavBar />
+            <NavBar userName={loggedInUser.firstName}/>
             
             <SearchForm />
 
