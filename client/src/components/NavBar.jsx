46a93ed77751437dc20eee8d41ac87955e7c8b70
alex k <alex.kuhn1123@gmail.com>
2020-10-22 14:48:54 -0700

    add userName to Dashboard navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import {Link, navigate} from '@reach/router';
 
 export default (props) => {
-    const [user, setUser] = useState(props.userName);
-    console.log();
+    const userName = props.userName;
+    console.log(userName);
 
     const logout = (e) => {
         e.preventDefault();
@@ -20,7 +20,7 @@ export default (props) => {
     return(
         <div className="d-flex">
             <Link to="/home" className="btn btn-outline-primary btn-lg m-4">Home</Link>
-    <h2 className="dark-font mr-auto mt-4"> Hey, {user}**insert user name**! </h2>
+    <h2 className="dark-font mr-auto mt-4"> Hey, {userName}! </h2>
             <Link to="/main" className="btn btn-outline-success btn-lg m-4">New List</Link>
             <button onClick={logout} className="btn btn-outline-danger btn-lg m-4">Logout</button>
         </div>
