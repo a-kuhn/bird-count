6daf40f05ac3c63a0c03c7c2165e98114f9a5c8d
alex k <alex.kuhn1123@gmail.com>
2020-10-22 20:29:56 -0700

    create NoLists for newly registered user dashboard

---
+++ client/src/components/NoLists.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => {
+
+
+    return(
+        <>
+            <h4 className="helper-msg-color">You haven't created any checklists yet! <br/>Click on the New List button to get started.</h4>
+            <div className="container">
+                <iframe 
+                    src="https://giphy.com/embed/X8ynfaP1ZwTvTbxH1b" 
+                    frameBorder="0" 
+                    className="" 
+                    allowFullScreen
+                    style={{height:400}}
+                >
+                </iframe>
+            </div>
+            <p><a href="https://giphy.com/gifs/cbc-schittscreek-schitts-creek-X8ynfaP1ZwTvTbxH1b">via GIPHY</a></p>
+        </>
+    );
+}
