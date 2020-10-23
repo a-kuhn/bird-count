675857d5d5c7f56af708df9670f4fbd2b99419c3
alex k <alex.kuhn1123@gmail.com>
2020-10-23 13:28:36 -0700

    rebuild for deployment

---
+++ client/build/precache-manifest.6d5734a06a96e5359589016e8e5e235a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f784cdc9830203316d51e492f4a2039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d508f46097bb5537e165",
+    "url": "/static/css/main.9cbab659.chunk.css"
+  },
+  {
+    "revision": "ff3575f1fa2800613a84",
+    "url": "/static/js/2.3833158b.chunk.js"
+  },
+  {
+    "revision": "bd4caed790789cbbaa8f9f5d33c292ce",
+    "url": "/static/js/2.3833158b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d508f46097bb5537e165",
+    "url": "/static/js/main.c2295994.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
