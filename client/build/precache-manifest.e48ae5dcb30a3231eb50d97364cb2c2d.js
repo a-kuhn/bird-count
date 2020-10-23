4808acea9ad05e4678b83bcc5da6e5179961ad68
alex k <alex.kuhn1123@gmail.com>
2020-10-22 20:45:55 -0700

    new build for deployment

---
+++ client/build/precache-manifest.e48ae5dcb30a3231eb50d97364cb2c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5d08f7806d9ca575fc6ce33ab0ab6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d450605565790a68e9",
+    "url": "/static/css/main.9cbab659.chunk.css"
+  },
+  {
+    "revision": "ff3575f1fa2800613a84",
+    "url": "/static/js/2.3833158b.chunk.js"
+  },
+  {
+    "revision": "bd4caed790789cbbaa8f9f5d33c292ce",
+    "url": "/static/js/2.3833158b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d450605565790a68e9",
+    "url": "/static/js/main.a2186a25.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
